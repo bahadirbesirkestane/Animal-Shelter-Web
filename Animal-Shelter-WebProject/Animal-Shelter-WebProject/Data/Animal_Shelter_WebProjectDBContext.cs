37df789a7805ba02ccfb4fb08d9991ab2e9ba68c
isim soyisim <bahadir_kestane@hotmail.com>
2023-08-01 13:03:26 +0300

    Veritabanı bağlantısı ve tabloların oluşturulması

---
+++ Animal-Shelter-WebProject/Animal-Shelter-WebProject/Data/Animal_Shelter_WebProjectDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Animal_Shelter_WebProject.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Animal_Shelter_WebProject.Data
+{
+    public class Animal_Shelter_WebProjectDBContext : DbContext
+    {
+        public Animal_Shelter_WebProjectDBContext(DbContextOptions<Animal_Shelter_WebProjectDBContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Adoption> Adoptions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
