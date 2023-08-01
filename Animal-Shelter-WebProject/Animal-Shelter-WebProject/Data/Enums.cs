6ba42ef07999406c61210b164ddbe1b5a0b9b20f
isim soyisim <bahadir_kestane@hotmail.com>
2023-08-01 13:02:55 +0300

    Gerekli seçenekler için enum lar eklendi

---
+++ Animal-Shelter-WebProject/Animal-Shelter-WebProject/Data/Enums.cs
@@ -0,0 +1,23 @@
+﻿namespace Animal_Shelter_WebProject.Data
+{
+    public enum Gender
+    {
+        Erkek = 0,
+        Dişi = 1,
+        Diger = 2,
+    }
+
+    public enum Kisirlik
+    {
+        Evet = 0,
+        Hayir = 1,
+        Belirtilmedi = 2,
+    }
+
+    public enum SurecDurumlari
+    {
+        TalepOlusturuldu = 0,
+        AdminOnayiBekleniyor = 1,
+        SahiplendirmeOnayandi = 2,
+    }
+}
