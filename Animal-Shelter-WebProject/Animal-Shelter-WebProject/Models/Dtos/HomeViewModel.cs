078cda6e9ea90b7bfe5a88aa481059763af5dfee
isim soyisim <bahadir_kestane@hotmail.com>
2023-08-02 12:53:11 +0300

    Dto düzenlemesi

--- Animal-Shelter-WebProject/Animal-Shelter-WebProject/Models/Dtos/HomeViewModel.cs
+++ Animal-Shelter-WebProject/Animal-Shelter-WebProject/Models/Dtos/HomeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Animal_Shelter_WebProject.Models.Dtos.Pets;
+﻿using Animal_Shelter_WebProject.Data;
+using Animal_Shelter_WebProject.Models.Dtos.Pets;
 using Animal_Shelter_WebProject.Models.Entities;
 
 namespace Animal_Shelter_WebProject.Models.Dtos
@@ -8,5 +9,10 @@ namespace Animal_Shelter_WebProject.Models.Dtos
         public User UserInfo { get; set; }
         public List<PetGetDto> Pets { get; set; }
         public Pet PetInfo { get; set; }
+        public List<Pet> PetList { get; set; }
+        public List<Adoption> Adoptions { get; set; }
+        public List<User> Users { get; set; }
+        public List<string> Temp { get; set; }
+        public List<SurecDurumlari> SurecTutucu { get; set; }
     }
 }
