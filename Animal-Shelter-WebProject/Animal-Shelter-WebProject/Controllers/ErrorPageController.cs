abff37b0418eee23cc27fe38ec12b7765befa0b4
isim soyisim <bahadir_kestane@hotmail.com>
2023-08-07 22:54:49 +0300

    404 Hata Sayfası Eklendi

---
+++ Animal-Shelter-WebProject/Animal-Shelter-WebProject/Controllers/ErrorPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Animal_Shelter_WebProject.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error1(int code)
+        {
+            return View();
+        }
+    }
+}
